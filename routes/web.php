9acaf34aa40fff7a8e5dd57f06947a5031effda5
vietdm <vietdau33@gmail.com>
2023-04-29 10:22:29 +0700

    dsa

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return response()->json(['status' => 'OK']);
-// });
+Route::get('/', function () {
+    return response()->json(['status' => 'OK']);
+});
