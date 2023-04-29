8499bca60a63608547e8384a9e3687ef7877e043
vietdm <vietdau33@gmail.com>
2023-04-29 10:23:48 +0700

    dsa

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('auth/login', [AuthController::class, 'login']);
-Route::put('auth/register', [AuthController::class, 'register']);
+// use App\Http\Controllers\Api\AuthController;
+// use Illuminate\Support\Facades\Route;
+//
+// Route::post('auth/login', [AuthController::class, 'login']);
+// Route::put('auth/register', [AuthController::class, 'register']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/', function () {
     return response()->json(['status' => 'OK']);
 });
+
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::put('auth/register', [AuthController::class, 'register']);
