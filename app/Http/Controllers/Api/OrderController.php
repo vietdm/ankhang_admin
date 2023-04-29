03c7012c4a3f742ac7d22c02be392d12a22ff214
vietdm <vietdau33@gmail.com>
2023-04-30 05:59:50 +0700

    dsa

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\Response;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\OrderRequest;
+use App\Models\Orders;
 
 class OrderController extends Controller
 {
-    public function order() {
-        //
+    public function order(OrderRequest $request) {
+        $order = new Orders();
+        $order->order = gettype($request->order) == 'string' ? $request->order : json_encode($request->order);
+        $order->user_id = $request->user_id;
+        $order->name = $request->name;
+        $order->phone = $request->phone;
+        $order->address = $request->address;
+        $order->note = $request->note;
+        $order->save();
+        return Response::success([]);
     }
 }
