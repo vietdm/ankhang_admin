243038b31242c74047dd37e409c834f6a18386fe
vietdm <vietdau33@gmail.com>
2023-05-04 00:20:56 +0700

    save

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -10,6 +10,8 @@ class RegisterRequest extends BaseRequest
     {
         return [
             'username' => 'required',
+            'email' => ['required', 'regex:/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/'],
+            'cccd' => ['required', 'regex:/^\d{9}(\d{3})?$/'],
             'phone' => ['required', 'regex:/^(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})$/', 'min:10'],
             'fullname' => 'required',
             'password' => 'required',
@@ -20,11 +22,12 @@ class RegisterRequest extends BaseRequest
     public function messages()
     {
         return [
-            ...parent::messages(),
             'phone.regex' => 'Số điện thoại không đúng định dạng!',
             'phone.min' => 'Số điện thoại không đúng định dạng!',
             'present_phone.regex' => 'Số điện thoại người giới thiệu không đúng định dạng!',
             'present_phone.min' => 'Số điện thoại người giới thiệu không đúng định dạng!',
+            'email.regex' => 'Email không hợp lệ!',
+            'cccd.regex' => 'Số CCCD phải là số có 9 hoặc 12 ký tự!',
         ];
     }
 }
