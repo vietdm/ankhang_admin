4ede039234939fdac0f7a79250ce08a6d6bc9e66
vietdau33 <vietdau33@gmail.com>
2023-05-05 00:16:14 +0700

    d

--- resources/views/mail/forgotpassword.blade.php
+++ resources/views/mail/forgotpassword.blade.php
@@ -1,7 +1,11 @@
 Xin chào,
-Đây là mã xác nhận thay đổi mật khẩu, mã này có thời gian hiệu lực là 10 phút. Sau 10 phút không được sử dụng thì mã sẽ tự hủy.
-<code>
-    {{ $token }}
-</code>
+<br />
+Đây là mã xác nhận thay đổi mật khẩu, mã này có thời gian hiệu lực là 10 phút.
+<br />
+Sau 10 phút không được sử dụng thì mã sẽ tự hủy.
+<br />
+<code>{{ $token }}</code>
+<br />
 Cảm ơn,
+<br />
 An Khang Group!
