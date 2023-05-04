0b7c27ec31c1bc133edba11d7f67c451b3b4c6ff
vietdau33 <vietdau33@gmail.com>
2023-05-05 02:23:39 +0700

    d

---
+++ app/Models/MissionList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MissionList extends Model
+{
+    use HasFactory;
+
+    protected $table = 'mission_list';
+}
