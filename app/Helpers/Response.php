d7e70dcebd7996a3fddf76c0b27713d8d5676a9f
vietdau33 <vietdau33@gmail.com>
2023-05-04 20:45:49 +0700

    d

--- app/Helpers/Response.php
+++ app/Helpers/Response.php
@@ -2,11 +2,13 @@
 
 namespace App\Helpers;
 
-class Response
-{
-    public static function success($data, $code = 200)
-    {
+class Response {
+    public static function success($data, $code = 200) {
         return response()->json($data, $code);
+    }
+
+    public static function badRequest($data) {
+        return response()->json($data, 400);
     }
 
     public static function Unauthorized($data = [])
