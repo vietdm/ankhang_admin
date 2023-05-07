d9eb6ffdf0946c9d04dd8cc13cace862079f98ad
vietdau33 <vietdau33@gmail.com>
2023-05-07 14:58:10 +0700

    a

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -26,8 +26,27 @@ class UserController extends Controller
 
     public function getTree(Request $request) {
         $userId = $request->user->id;
-        $user = Users::select(['id', 'email', 'phone', 'fullname', 'present_phone'])->whereId($userId)->first()->toArray();
-        $userTree = UserUtil::getTreeUser($user);
+        $user = Users::select(['id', 'phone', 'fullname'])->whereId($userId)->first()->toArray();
+        $userTree = [$user];
+
+        //get level 1
+        $userLevel1 = Users::select(['id', 'phone', 'fullname'])->wherePresentPhone($user['phone'])->get()->toArray();
+
+        //get level2
+        foreach ($userLevel1 as $userlv1) {
+            $userLevel2 = Users::select(['id', 'phone', 'fullname'])->wherePresentPhone($userlv1['phone'])->get()->toArray();
+
+            //get level 3
+            foreach ($userLevel2 as $userlv2) {
+                $userLevel3 = Users::select(['id', 'phone', 'fullname'])->wherePresentPhone($userlv2['phone'])->get()->toArray();
+                $userLevel2['children'] = $userLevel3;
+            }
+
+            $userLevel1['children'] = $userLevel2;
+        }
+
+        $userTree['children'] = $userLevel1;
+
         return Response::success([
             'tree' => $userTree
         ]);
