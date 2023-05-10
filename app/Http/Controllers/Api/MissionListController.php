55674b70ed63b081ca033393cbac4aca1269c2c4
vietdau33 <vietdau33@gmail.com>
2023-05-11 00:38:32 +0700

    a

--- app/Http/Controllers/Api/MissionListController.php
+++ app/Http/Controllers/Api/MissionListController.php
@@ -19,6 +19,7 @@ class MissionListController extends Controller
             ->whereUserId($userId)
             ->where('date', Carbon::now()->format('Y-m-d'))
             ->get();
+        logger(Carbon::now()->format('Y-m-d'));
         $missionList = MissionList::whereType($type)->get();
         return Response::success([
             'limit' => 5 - $missionWithTypeOfUser->count(),
