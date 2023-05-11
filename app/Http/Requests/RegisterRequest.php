dc792870bfbe7e0e3c8a54b17f2922ddce81c5a9
vietdau33 <vietdau33@gmail.com>
2023-05-11 19:54:29 +0700

    d

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -9,7 +9,7 @@ class RegisterRequest extends BaseRequest
     public function rules(): array
     {
         return [
-            'username' => ['required', 'regex:/^[a-z][a-z0-9]{3,31}$/'],
+            'username' => ['required', 'regex:/^[a-zA-Z][a-zA-Z0-9]{3,31}$/'],
             'email' => ['required', 'regex:/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/'],
             'phone' => ['required', 'regex:/^(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})$/', 'min:10'],
             'fullname' => 'required',
@@ -21,7 +21,7 @@ class RegisterRequest extends BaseRequest
     public function messages()
     {
         return [
-            'username.regex' => 'Username phải có ít nhất 4 ký tự, viết liền, chỉ bao gồm số và chữ thường!',
+            'username.regex' => 'Username phải có ít nhất 4 ký tự, viết liền, chỉ bao gồm chữ thường, chữ hoa và số!',
             'phone.regex' => 'Số điện thoại không đúng định dạng!',
             'phone.min' => 'Số điện thoại không đúng định dạng!',
             'email.regex' => 'Email không hợp lệ!',
