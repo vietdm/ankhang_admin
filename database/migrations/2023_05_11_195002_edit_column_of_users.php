4cd1390ef186fbedb6fb4fd8ff43dfb2cb634f46
vietdau33 <vietdau33@gmail.com>
2023-05-11 19:51:03 +0700

    d

---
+++ database/migrations/2023_05_11_195002_edit_column_of_users.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('ALTER TABLE users MODIFY cccd varchar(255) default null;');
+    }
+};
