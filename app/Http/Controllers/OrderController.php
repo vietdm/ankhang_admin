1e77d20c1e59d05842b288f31cc8505f9143da66
vietdm <vietdau33@gmail.com>
2023-05-12 17:36:45 +0700

    ad

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\Response;
+use App\Helpers\Telegram;
 use App\Models\Orders;
+use App\Models\Users;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -36,6 +38,25 @@ class OrderController extends Controller
         try {
             $order->accept();
             DB::commit();
+
+            $product = Products::whereIn('id', $order->product_id)->first();
+            $totalPrice = number_format($order->total_price);
+
+            $mgs = <<<text
+Có đơn hàng mới!
+==============
+Họ tên: $order->name
+Số điện thoại: $order->phone
+Địa chỉ: $order->address
+Ghi chú: $order->note
+=============
+Tên sản phẩm: $product->title
+Số lượng: $order->quantity
+Tổng giá: $totalPrice
+text;
+
+            Telegram::pushMgs($mgs);
+
             return Response::success('Thành công!');
         } catch (Exception|PDOException $e) {
             logger($e);
